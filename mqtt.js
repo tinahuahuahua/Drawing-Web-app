5c9de0b5ee6e9da46bb67ab225648c72a81ab3a3
LAPTOP-ALK568R7\Tina Hua <tinachang1007@gmail.com>
2022-08-15 22:39:25 +0800

    基礎已完成

--- mqtt.js
+++ mqtt.js
@@ -2,9 +2,9 @@ const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
 
 const host = 'ws://broker.emqx.io:8083/mqtt'
 
-const topic1 = "color1_1"
-const topic2 = "color2_1"
-const topicmix = "mixcolor_1"
+const topic1 = "nccudct_color1_1"
+const topic2 = "nccudct_color2_1"
+const topicmix = "nccudct_mixcolor_1"
 
 const options = {
   keepalive: 30,
@@ -46,21 +46,55 @@ client.on('connect', () => {
 
 client.on('message', (topic, message, packet) => { //判斷哪個topic傳入的值
   if(topic == topic1){
-    console.log("print1!");
-    $("#color1").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
-    $("#color1").css("background","#"+message.toString());
+      if(message.toString() =="delete1")
+      {
+        console.log("delete1!");
+        $("#color1").removeAttr('onclick');
+        $("#color1").css("border-width","2px");
+        $("#color1").css("background","rgb(210,210,210)");
+        $("#color1").css("pointer-events","none");
+      }
+      else
+      {
+        console.log("print1!");
+        $("#color1").css("pointer-events","all");
+        $("#color1").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
+        $("#color1").css("background","#"+message.toString());
+      }
   }
   else if(topic == topic2){
-    console.log("print2!");
-    $("#color2").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
-    $("#color2").css("background","#"+message.toString());
+    if(message.toString()=="delete2")
+      {
+        console.log("delete2!");
+        $("#color2").removeAttr('onclick');
+        $("#color2").css("border-width","2px");
+        $("#color2").css("background","rgb(210,210,210)");
+        $("#color2").css("pointer-events","none");
+      }
+      else
+      {
+        console.log("print2!");
+        $("#color2").css("pointer-events","all");
+        $("#color2").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
+        $("#color2").css("background","#"+message.toString());
+      }
   }
   else if(topic == topicmix){
-    console.log("mix!");
-    $("#mixcolor").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
-    $("#mixcolor").css("background","#"+message.toString());
-  }
+    if(message.toString() == "deletemix"){
+      console.log("deletemix!");
+        $("#mixcolor").removeAttr('onclick');
+        $("#mixcolor").css("border-width","2px");
+        $("#mixcolor").css("background","rgb(210,210,210)");
+        $("#mixcolor").css("pointer-events","none");
+    }
+    else{
+        console.log("mix!");
+        $("#mixcolor").css("pointer-events","all");
+        $("#mixcolor").removeAttr('onclick').attr("onclick","changeColorAndSize(\'#"+message.toString()+"\',10)");
+        $("#mixcolor").css("background","#"+message.toString());
+    }
 
+  }
   console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic)
 })
 
